b91120d11ba6fa5e8af077012c7490ff9d483292
unknown <sachinmewar800@gmail.com>
2021-06-05 07:58:13 +0530

    Added Image in Side Navigation

---
+++ src/components/SideNavigationBar/Image.js
@@ -0,0 +1,10 @@
+import './Image.css';
+
+const Image = () => {
+   return (
+      <div className='profilePic-container'>
+         <img src='/Ell.jpg' alt='Problem While loading pic' />
+      </div>
+   )
+}
+export default Image;
