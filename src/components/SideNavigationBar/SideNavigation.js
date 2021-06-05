157e9f0e3a94621847e51e0e5bef9bddf441d8fd
unknown <sachinmewar800@gmail.com>
2021-06-05 18:29:25 +0530

    Added Side Navigation bar

---
+++ src/components/SideNavigationBar/SideNavigation.js
@@ -0,0 +1,16 @@
+
+import './SideNavigation.css';
+import Image from './Image';
+import About from './About';
+
+const SideNavigation = () => {
+   return (
+      <div className='sideNavigation-container'>
+         <div>
+            <Image />
+         </div>
+      </div>
+   );
+}
+
+export default SideNavigation;
